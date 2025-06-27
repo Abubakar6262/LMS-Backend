cf2cc6d1482386400e1ed2a1058f925df55d742c
abu bakar <abubkarmukhtar44@gmail.com>
2025-06-27 22:39:47 +0500

    test

--- models/courseModel.js
+++ models/courseModel.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose")
 
 
 const courseSchema = new mongoose.Schema({
-   title:{
-    type:String,
-    require:[true,"Please enter course tile"],
-    minlenght:[3,"Title must has at leat three char"]
-   } 
+   title: {
+      type: String,
+      require: [true, "Please enter course tile"],
+      minlenght: [3, "Title must has at leat three char"]
+   },
+   description: {
+      type: String,
+
+   }
+      
 
 })
 
